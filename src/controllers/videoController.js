3c628609facbf010a54230cdc8c6e70defacb6b0
Yook JIWON <jwyook27@naver.com>
2021-11-16 18:37:44 +0900

    #6.1 Async Await

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,9 +1,12 @@
 import Video from "../models/Video";
 
-export const home = (req, res) => {
-  Video.find({}, (error, videos) => {
+export const home = async (req, res) => {
+  try {
+    const videos = await Video.find({});
     res.render("home", { pageTitle: "Home", videos: [] });
-  });
+  } catch {
+    return res.render("server-error");
+  }
 };
 
 export const watch = (req, res) => {
