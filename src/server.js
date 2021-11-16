9ee971a85b10d1942139a82299d511f3aa070ac3
Yook JIWON <jwyook27@naver.com>
2021-11-16 14:53:01 +0900

    #6 First Query

--- src/server.js
+++ src/server.js
@@ -4,8 +4,6 @@ import globalRouter from "./routers/globalRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 
-const PORT = 4000;
-
 const app = express();
 const logger = morgan("dev");
 
@@ -18,7 +16,4 @@ app.use("/", globalRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
 
-const handleListening = () =>
-  console.log(`Server listening on port http://localhost:${PORT}`);
-
-app.listen(PORT, handleListening);
+export default app;
