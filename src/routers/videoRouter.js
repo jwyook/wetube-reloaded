9f68017966a66d062260e965fefc7b886388810f
Yook JIWON <jwyook27@naver.com>
2021-11-22 17:28:39 +0900

    #6.7 Delete Video

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -2,10 +2,9 @@ import express from "express";
 import {
   watch,
   getUpload,
-  postUpload,
   getEdit,
   postEdit,
-  upload,
+  postUpload,
   deleteVideo,
 } from "../controllers/videoController";
 
@@ -13,6 +12,7 @@ const videoRouter = express.Router();
 
 videoRouter.get("/:id([0-9a-f]{24})", watch);
 videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
+videoRouter.route("/:id([0-9a-f]{24})/delete").get(deleteVideo);
 videoRouter.route("/upload").get(getUpload).post(postUpload);
 
 export default videoRouter;
