1ad2b12c09127bcc4b4daab2274f14eb2cc39b78
Yook JIWON <jwyook27@naver.com>
2021-11-25 01:39:30 +0900

    #7.4 Session & Logged In User

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -59,7 +59,8 @@ export const postLogin = async (req, res) => {
       errorMessage: "Wrong password",
     });
   }
-  console.log("Log User In ! Coming Soon!");
+  req.session.loggedIn = true;
+  req.session.user = user;
   return res.redirect("/");
 };
 
