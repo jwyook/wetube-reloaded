d05858df781bc2c31833bb2d6e554b71c7c96a1a
Yook JIWON <jwyook27@naver.com>
2021-12-27 17:45:45 +0900

    #16.7 Logout Bugfix

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -141,8 +141,6 @@ export const finishGithubLogin = async (req, res) => {
 
 export const logout = (req, res) => {
   req.session.destroy();
-  req.flash("info", "Bye Bye");
-
   return res.redirect("/");
 };
 
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -14,6 +14,7 @@ import {
   publicOnlyMiddleware,
   avatarUpload,
 } from "../middlewares";
+
 const userRouter = express.Router();
 
 userRouter.get("/logout", protectorMiddleware, logout);
@@ -22,7 +23,6 @@ userRouter
   .all(protectorMiddleware)
   .get(getEdit)
   .post(avatarUpload.single("avatar"), postEdit);
-
 userRouter
   .route("/change-password")
   .all(protectorMiddleware)
