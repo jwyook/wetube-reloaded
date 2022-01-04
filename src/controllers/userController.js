8f6415a560616d6df016957a3c782bff741dd374
Yook JIWON <jwyook27@naver.com>
2022-01-04 22:18:04 +0900

    s3 uploads

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -176,11 +176,12 @@ export const postEdit = async (req, res) => {
       });
     }
   }
-  const isHeroku = process.env.NODE_ENV === "production";
+  // const isHeroku = process.env.NODE_ENV === "production";
   const updatedUser = await User.findByIdAndUpdate(
     _id,
     {
-      avatarUrl: file ? (isHeroku ? file.location : file.path) : avatarUrl,
+      // avatarUrl: file ? (isHeroku ? file.location : file.path) : avatarUrl,
+      avatarUrl: file ? file.location : avatarUrl,
       name,
       email,
       username,
