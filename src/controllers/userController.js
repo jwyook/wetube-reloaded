cdfc1a3046c6b95f1603794452f85cbc96185412
Yook JIWON <jwyook27@naver.com>
2022-01-04 22:55:05 +0900

    isHeroku

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -176,12 +176,11 @@ export const postEdit = async (req, res) => {
       });
     }
   }
-  // const isHeroku = process.env.NODE_ENV === "production";
+  const isHeroku = process.env.NODE_ENV === "production";
   const updatedUser = await User.findByIdAndUpdate(
     _id,
     {
-      // avatarUrl: file ? (isHeroku ? file.location : file.path) : avatarUrl,
-      avatarUrl: file ? file.location : avatarUrl,
+      avatarUrl: file ? (isHeroku ? file.location : file.path) : avatarUrl,
       name,
       email,
       username,
